19c43f77e489adb3a58eb41edebef3772fcf11f8
raselcsedev <raselcse.dev@gmail.com>
2022-08-15 22:50:50 +0600

    protected route added and dashboard added

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import BucketGirl from "../assets/image/bucketgirl.png";
 
 const About = () => {
     return (
-        <div>
-            <h1>This is About</h1>
+        <div className='mt-32'>
+            <h2>About</h2>
+            <div className='h-[60vh] shrink-0'>
+            <img src={BucketGirl} class='h-full' alt='' />
+          </div>
+            
         </div>
     );
 };
