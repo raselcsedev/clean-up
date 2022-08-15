7afb3aed6811d3af679b15e2f2e69b1aa30ab11a
raselcsedev <raselcse.dev@gmail.com>
2022-08-15 09:19:51 +0600

    theme changes- light and dark

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Contract from "./pages/Contract";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Login from "./pages/Login";
+
 
 
 function App() {
   return (
-    <div >
-      <Navbar />
-    </div>
+    <>
+      <Navbar>
+        <Routes>
+          <Route path="/" element={<Home />} ></Route>
+          <Route path="/about" element={<About />} ></Route>
+          <Route path="/contact" element={<Contract />} ></Route>
+          <Route path="/services" element={<Services />} ></Route>
+          <Route path="/login" element={<Login />} ></Route>
+        </Routes>
+      </Navbar>
+    </>
   );
 }
 
